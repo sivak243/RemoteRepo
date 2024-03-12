316525bc4dbe456298c320338b5f38a870835ae6
unknown <sivakumr243@yahoo.co.in>
2024-03-12 05:39:45 +0530

    Created SG with SSH -adding V1 version

--- vpc_main.tf
+++ vpc_main.tf
@@ -27,3 +27,33 @@ resource "aws_security_group" "allow_http" {
     Name = "allow_http"
   }
 }
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id=var.VPC_ID
+  
+
+ 
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+   
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
